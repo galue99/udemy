c2fb1f186a81df7a29544f3215b11423a6e07eac
edgar <eespinetti_99@hotmail.com>
2016-09-14 07:56:40 -0430

    ...

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('\'Allo \'Allo!');
+
+
+var fruta = {nombre: 'Manzana', color: "Rojo", peso: "250 Gramos"};
+
+var descripcion = `La ${fruta.nombre} es de color ${fruta.color} y tiene un peso de ${fruta.peso}`;
+
+console.log('Descripcion: ' + descripcion);
